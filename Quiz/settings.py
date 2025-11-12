4a369792409bbd87cadd9d19e1f1fba91b144b0f
Dhruv Meta <141990851+dhruvmeta@users.noreply.github.com>
2025-11-12 16:13:33 +0530

    Allowed Host

--- Quiz/settings.py
+++ Quiz/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q$a*e1+zb_0u*sqysdk&t-iv*jb5%*!xjti%$qfnor!2f6#der
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
